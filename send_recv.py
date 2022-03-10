694aa53d9d9746d317e6ea691a050996a9fb5513
Krzysztof Nalepa <krzysztof.nalepa@appliscale.io>
2022-03-10 15:37:29 +0100

    send_recv

---
+++ send_recv.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+
+if rank == 0:
+   print("rank0")
+   data = ('abc',123)
+   comm.send(data, dest=1)
+elif rank == 1:
+   print("rank1")
+   data = comm.recv(source=0)
+   print(data)
+else:
+   print("Expected only two nodes")
