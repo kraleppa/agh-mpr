19f27e79924eef38c4bb6c2719f1ac31e3f56832
Krzysztof Nalepa <plgkralepa@login02.prometheus.cyfronet.pl>
2022-03-19 14:29:52 +0100

    settings updated

--- lab2/run_pr.sh
+++ lab2/run_pr.sh
@@ -1,6 +1,16 @@
-rm results.csv
+#!/bin/bash -l
+#SBATCH --nodes 1
+#SBATCH --ntasks 12
+#SBATCH --time=12:00:00
+#SBATCH --partition=plgrid
+#SBATCH --account=plgmpr22
+
 values=4000000,34641016,300000000
 # values=400,3460,30000
+
+module add plgrid/tools/openmpi
+module add plgrid/libs/python-mpi4py/3.0.0-python-2.7
+rm results.csv
 
 for val in ${values//,/ }
 do
@@ -8,7 +18,7 @@ do
     do
         for threads in {1..12}
         do
-            mpiexec -machinefile nodes -np $threads ./pi.py $val >> results.csv
+            mpiexec -np $threads ./pi.py $val >> results.csv
         done
     done
 done
