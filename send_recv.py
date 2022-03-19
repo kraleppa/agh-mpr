ed01d8567a5b6a153f4cdb4f6157d86c5a13487b
Krzysztof Nalepa <nalepa@vnode-01.dydaktyka.icsr.agh.edu.pl>
2022-03-19 10:35:09 +0100

    basic ping pong

--- send_recv.py
+++ send_recv.py
@@ -1,15 +1,41 @@
 #!/usr/bin/env python
 from mpi4py import MPI
+
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
+test_count = 10000
+bytes_to_send = 1000000
+message = "A" * bytes_to_send
+
+def ping_pong():
+   comm.Barrier()
+   start = MPI.Wtime()
+
+   if rank == 0:
+      comm.send(message, dest=1)
+      comm.recv(source=1)
+   elif rank == 1:
+      data = comm.recv(source=0)
+      comm.send(data, dest=0)
+
+   stop = MPI.Wtime()
+   
+   return stop - start
+
+
+
+
+summed_time = 0
+
+for i in range(test_count):
+   time = ping_pong()
+   summed_time = summed_time + time
+
 if rank == 0:
-   print("rank0")
-   data = ('abc',123)
-   comm.send(data, dest=1)
-elif rank == 1:
-   print("rank1")
-   data = comm.recv(source=0)
-   print(data)
-else:
-   print("Expected only two nodes")
+   result = summed_time / test_count
+   speed_in_bits = bytes_to_send * 8 / result 
+
+   print "Time per message:", result, "s"
+   print "Message size:", bytes_to_send / 1000000, "MB"
+   print "Speed:", speed_in_bits / 1000000, "Mb/s" 
