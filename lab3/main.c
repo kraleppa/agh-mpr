88f82406d050cd80b554da3a18e4c43907144dab
Krzysztof Nalepa <nalepa@vnode-01.dydaktyka.icsr.agh.edu.pl>
2022-03-31 15:12:45 +0200

    basic for added

--- lab3/main.c
+++ lab3/main.c
@@ -6,11 +6,21 @@
 int main(int argc, char* argv[])
 {
  
+    int size = 16;
+    int *tab = calloc(size, sizeof(int));
     // Beginning of parallel region
     #pragma omp parallel
     {
-        printf("Hello World... from thread = %d\n", omp_get_thread_num());
+        for (int i = omp_get_thread_num(); i < size; i=i+4)
+        {
+            tab[i] = omp_get_thread_num();
+        }
     }
+
+    for (int i = 0; i < size; i++){
+        printf("%d", tab[i]);
+    }
+    printf("\n"); 
     // Ending of parallel region
     return 0;
 }
