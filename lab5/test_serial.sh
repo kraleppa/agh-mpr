6d650354ef11d1fcbe20b07cf58c43ff2a5f6f35
Krzysztof Nalepa <krzysztof.nalepa@appliscale.io>
2022-05-15 16:01:24 +0200

    param changed

--- lab5/test_serial.sh
+++ lab5/test_serial.sh
@@ -5,7 +5,7 @@ do
     for i in {1..10}
     do
         echo "size $size iteration $i"
-        ts=$(date +%s%N) ; python3 word_count.py ./get_data.sh ; tt=$((($(date +%s%N) - $ts)/1000000)) ; echo "$size;$tt" >> res.txt
+        ts=$(date +%s%N) ; python3 word_count.py ./data.txt ; tt=$((($(date +%s%N) - $ts)/1000000)) ; echo "$size;$tt" >> res.txt
     done;
     rm data.txt
 done;
